dd70ac1debc9ecd1dc5089d2696d2ec4c47147e4
tonyxie <tonyxie_0419@qq.com>
2017-08-23 16:34:33 +0800

    Kotlin中lazyProperty的用法

---
+++ LazyProperty.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    var initialized = false
+    val lazyProperty = LazyProperty({ initialized = true;42 })
+    val result: Int = lazyProperty.lazy
+
+    println(initialized)
+
+    println(result)
+}
+
+class LazyProperty(val initializer: () -> Int) {
+    var value: Int? = null
+    val lazy: Int
+        get() {
+            if (value == null) {
+                value = initializer()
+            }
+            return value!!
+        }
+}
