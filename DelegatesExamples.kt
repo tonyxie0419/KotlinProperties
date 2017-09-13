ab3d454bd162a2c4ca80cee5b006ae47baac7bb8
tonyxie <tonyxie_0419@qq.com>
2017-09-13 11:24:31 +0800

    Kotlin中lazyProperty的用法

--- DelegatesExamples.kt
+++ DelegatesExamples.kt
@@ -1,9 +1,19 @@
-class LazyProperty2(val initializer: () -> Int) {
+class LazyProperty2(private val initializer: () -> Int) {
     val lazyValue: Int by lazy(initializer)
 }
 
 fun main(args: Array<String>) {
     var initialized2 = false
-    val lazyProperty2 = LazyProperty2({ initialized2 = true;42 })
-    println(lazyProperty2.lazyValue)
+//    val lazyProperty2 = LazyProperty2({ initialized2 = true;42 })
+//    println(lazyProperty2.lazyValue)
+//    println(initialized2)
+    val man = Man({ initialized2 = true;"xie" }, { initialized2 = false;20 })
+    println(man.lazyName)
+    println(man.lazyAge)
+    println(initialized2)
+}
+
+class Man(val name: () -> String, val age: () -> Int) {
+    val lazyName: String by lazy(name)
+    val lazyAge: Int by lazy(age)
 }
